d383827f8c9b8d3ade174d49495cf99dd5e9cf04
Muruganantham <43311180+muruganantham1009@users.noreply.github.com>
2023-04-16 18:55:00 -0400

    api: pagination and error handling on getBookings and addBooking urls

--- api/src/server.ts
+++ api/src/server.ts
@@ -18,43 +18,72 @@ api.get('/getBookingLocations', (req, res) => {
   res.json(Object.values(BookingLocations));
 });
 
-api.get('/getBookings', async (res, req) => {
-  const db = await connect()
-  // Catch errors
+api.get('/getBookings', async (req, res) => {
+  const db = await connect();
+  const rowsPerPage = 100;
+  const page = parseInt(req.query.page?.toString() || '1');
+
   try {
-    const { rows } = await db.query(`SELECT * FROM bookings`)
-    req.json(rows)
+    const { rows } = await db.query(`
+      SELECT *
+      FROM bookings
+      LIMIT $1
+      OFFSET $2
+    `, [rowsPerPage, (page - 1) * rowsPerPage]);
+
+    const totalCount = await db.query('SELECT COUNT(*) FROM bookings');
+    const totalRows = parseInt(totalCount.rows[0].count);
+
+    const totalPages = Math.ceil(totalRows / rowsPerPage);
+
+    res.json({
+      bookings: rows,
+      totalPages,
+      currentPage: page,
+    });
   } catch (ex) {
-    req.json({ error: `Database Error!`, ...ex })
+    res.json({ error: `Database Error!`, ...ex });
   }
-})
+});
+
 
 api.post('/addBooking', async (req, res) => {
-  const db = await connect()
-  // Catch errors
+  const db = await connect();
+  
   try {
-    const { bookingDate, username, location } = req.body
+    const { bookingDate, username, location } = req.body;
 
-    const { rows: existing } = await db.query(`SELECT * FROM bookings WHERE "bookingDate"='${bookingDate}' AND "location"='${location}'`)
+    const { data: locations } = await axios.get('http://localhost:4000/api/getBookingLocations');
+
+    if (!locations.includes(location)) {
+      res.status(400).json({ error: `Booking not available in ${location}. Please contact us for further assistance.` });
+      return;
+    }
+
+    // Use parameterized values in the query
+    const { rows: existing } = await db.query(`SELECT * FROM bookings WHERE "bookingDate" = $1 AND "location" = $2`, [bookingDate, location]);
 
     if (existing.length > 0) {
-      throw Error('Booking already exists for that date!')
-    }
+      res.status(409).json({ error: `Booking already exists for the Booking date ${bookingDate} on the selected location ${location}!` });
+      return;
+    }    
 
     let id;
     (async () => {
       do {
-        id = uuid()
-      } while (!(await db.query(`SELECT * FROM bookings WHERE "id" = '${id}'`)).rowCount)
-    })()
+        id = uuid();
+      } while (!(await db.query(`SELECT * FROM bookings WHERE "id" = $1`, [id])).rowCount);
+    })();
 
-    await db.query(`INSERT INTO bookings ("id", "createdDate", "bookingDate", "location", "username") VALUES ('${id}', NOW(), '${bookingDate}', '${location}', '${username}')`)
+    await db.query(`INSERT INTO bookings ("id", "createdDate", "bookingDate", "location", "username") VALUES ($1, CURRENT_TIMESTAMP, $2, $3, $4)`, [id, bookingDate, location, username]);
 
     res.json({ message: 'Booking Created!' });
   } catch (ex) {
-    res.json({ error: `Database Error!`, ...ex })
+    console.error(ex);
+    res.status(500).json({ error: `Database Error!`, ...ex });
   }
 });
+
 
 api.get('/deleteBooking', async (req, res) => {
   const db = await connect();
@@ -63,7 +92,7 @@ api.get('/deleteBooking', async (req, res) => {
     await db.query(`DELETE FROM bookings WHERE "id"='${req.query.id}'`)
     res.json({ message: 'Booking Deleted!' })
   } catch (ex) {
-    res.json({ error: `Database Error!`, ...ex })
+    res.json({ error: `Database Error when deleteing the booking id ${req.query.id}!`, ...ex })
   }
 })
 
