e6330bafc5e5bb11c604b397192cafa81e45c159
Muruganantham <43311180+muruganantham1009@users.noreply.github.com>
2023-04-16 18:27:26 -0400

    db: fixing db init-fixing column type and adding constraints and seed-adding more record

--- api/migrations/1-init.sql
+++ api/migrations/1-init.sql
@@ -1,7 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE "bookings" (
-  "id" TEXT PRIMARY KEY, 
-  "createdDate" TEXT, 
-  "bookingDate" TEXT,
-  "location" TEXT,
-  "username" TEXT);
+  "id" UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  "createdDate" TIMESTAMP NOT NULL,
+  "bookingDate" TIMESTAMP NOT NULL,
+  "location" VARCHAR(255) NOT NULL,
+  "username" VARCHAR(15) NOT NULL,
+  CHECK (location ~* '^[a-z]+$'::text),
+  CHECK (username ~* '^[\w@]+$'::text)
+);
--- api/migrations/2-seed.sql
+++ api/migrations/2-seed.sql
@@ -2,8 +2,27 @@
 /* Some example data to seed the app for testing */
 
 INSERT INTO 
-  "bookings" ("id", "createdDate", "bookingDate", "location", "username")
+  "bookings" ("createdDate", "bookingDate", "location", "username")
 VALUES
-  ('1', NOW(), 'Mon Nov 15 2021 00:00:00', 'Victoria', 'will'),
-  ('2', NOW(), 'Sun Nov 14 2021 00:00:00', 'Vancouver', 'noel'),
-  ('3', NOW(), 'Mon Nov 15 2021 00:00:00', 'Vancouver', 'david');
+  (NOW(), to_timestamp('Mon Nov 15 2021 00:00:00', 'Dy Mon DD YYYY HH24:MI:SS'), 'Victoria', 'will'),
+  (NOW(), to_timestamp('Sun Nov 14 2021 00:00:00', 'Dy Mon DD YYYY HH24:MI:SS'), 'Vancouver', 'noel'),
+  (NOW(), to_timestamp('Mon Nov 15 2021 00:00:00', 'Dy Mon DD YYYY HH24:MI:SS'), 'Vancouver', 'david');
+
+INSERT INTO "bookings" ("createdDate", "bookingDate", "location", "username")
+SELECT
+  NOW() AS "createdDate",
+  (NOW() + (random() * INTERVAL '60' DAY)) AS "bookingDate",
+  CASE (random() * 9)::int
+    WHEN 0 THEN 'Vancouver'
+    WHEN 1 THEN 'Victoria'
+    WHEN 2 THEN 'Calgary'
+    WHEN 3 THEN 'Toronto'
+    WHEN 4 THEN 'Montreal'
+    WHEN 5 THEN 'Ottawa'
+    WHEN 6 THEN 'Halifax'
+    WHEN 7 THEN 'Winnipeg'
+    WHEN 8 THEN 'Saskatoon'
+    ELSE 'Regina'
+  END AS "location",
+  substr(md5(random()::text), 1, 8) AS "username"
+FROM generate_series(1, 1000);
